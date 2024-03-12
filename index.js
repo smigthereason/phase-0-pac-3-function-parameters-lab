c6634895ce925d55755002d952731947ecbd3176
victor@victor <victor.dmaina@gmail.com>
2024-03-12 17:22:13 +0300

    add

---
+++ index.js
@@ -0,0 +1,29 @@
+function introduction(name) {
+  console.log(`Hi, my name is Aki.`);
+  return `Hi, my name is ${name}.`;
+}
+
+function introduction(name) {
+  console.log(`Hi, my name is Samip.`);
+  return `Hi, my name is ${name}.`;
+}
+
+function introductionWithLanguage(name = "Aki", language = "Ember.js") {
+if (typeof string === "name, language")
+
+  console.log(
+    `Hi, my name is ${name} and I am learning to program in ${language}.`
+  );
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(name = "Gracie", language = "JavaScript") {
+
+    //console.log(`Hello, my name is ${name} and I am learning to program in ${language}.`);
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  } 
+
+
+// Example usage
+introductionWithLanguageOptional("John"); // Uses default language "JavaScript"
+introductionWithLanguageOptional("Alice", "Python"); // Provides a custom language
